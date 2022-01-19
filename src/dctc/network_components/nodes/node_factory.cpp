d342021d9f738f229646d74241893b75d0da79c3
lamductan <lamductan@gmail.com>
2022-01-19 00:16:04 -0600

    add Tran's algorithm skeleton

--- src/dctc/network_components/nodes/node_factory.cpp
+++ src/dctc/network_components/nodes/node_factory.cpp
@@ -1,6 +1,7 @@
 #include "dctc/network_components/nodes/node.h"
 #include "dctc/network_components/nodes/MST_node.h"
 #include "dctc/network_components/nodes/node_factory.h"
+#include "dctc/algorithms/connectivity/Tran/MST_node_Tran.h"
 
 
 Node* NodeFactory::createNode(Node* node, GraphNodeType graph_node_type) {
@@ -8,6 +9,8 @@ Node* NodeFactory::createNode(Node* node, GraphNodeType graph_node_type) {
         return new Node(*node);
     } else if (graph_node_type == MST_GRAPH_NODE) {
         return new MSTNode((MSTNode*) node);
+    } else if (graph_node_type == MST_GRAPH_NODE_TRAN) {
+        return new MSTNodeTran((MSTNode*) node);
     }
     return nullptr;
 }
