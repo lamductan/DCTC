aee0ad52678db6db3fbb73a620d9fb3755ea5e6b
lamductan <lamductan@gmail.com>
2022-01-23 22:36:36 -0600

    debug orient 4 nodes in a line

--- build/third_party/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tdlam/dev/phd/research/2021/2021-09b/DCTC/third_party/googletest/googlemock
-# Build directory: /home/tdlam/dev/phd/research/2021/2021-09b/DCTC/build/third_party/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
