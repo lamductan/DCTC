ea91514018c2bb4beb2335d532fd73ce8aa258af
lamductan <lamductan@gmail.com>
2022-01-31 01:53:06 -0600

    fix bug in ShortEdgeFirst

--- visualization/visualize.cpp
+++ visualization/visualize.cpp
@@ -14,11 +14,13 @@ void test2() {
     std::string relays_mst_graph_Aschner_path = DATA_DIR + "relays_mst_graph_Aschner.txt";
     std::string relays_mst_graph_Tran_path = DATA_DIR + "relays_mst_graph_Tran.txt";
     std::string relays_mst_graph_Lam_LEF_path = DATA_DIR + "relays_mst_graph_Lam_LEF.txt";
+    std::string relays_mst_graph_Lam_SEF_path = DATA_DIR + "relays_mst_graph_Lam_SEF.txt";
 
     std::string mst_graph_img_path = DATA_DIR + "mst_graph.png";
     std::string relays_mst_graph_Aschner_img_path = DATA_DIR + "relays_mst_graph_Aschner.png";
     std::string relays_mst_graph_Tran_img_path = DATA_DIR + "relays_mst_graph_Tran.png";
     std::string relays_mst_graph_Lam_LEF_img_path = DATA_DIR + "relays_mst_graph_Lam_LEF.png";
+    std::string relays_mst_graph_Lam_SEF_img_path = DATA_DIR + "relays_mst_graph_Lam_SEF.png";
 
     Instance instance = Instance::load(instance_path);
     std::vector<std::pair<long double, long double>> targets = instance.getTargets();
@@ -42,6 +44,11 @@ void test2() {
     Graph relays_mst_graph_Lam_LEF = Graph::load(relays_mst_graph_Lam_LEF_path);
     relays_mst_graph_Lam_LEF.draw_targets(targets);
     relays_mst_graph_Lam_LEF.draw(relays_mst_graph_Lam_LEF_img_path);
+
+    /* draw relays mst graph Lam_SEF */
+    Graph relays_mst_graph_Lam_SEF = Graph::load(relays_mst_graph_Lam_SEF_path);
+    relays_mst_graph_Lam_SEF.draw_targets(targets);
+    relays_mst_graph_Lam_SEF.draw(relays_mst_graph_Lam_SEF_img_path);
 }
 
 int main() {
